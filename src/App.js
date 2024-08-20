4b86754f86b27b7578f2c5259868f2b4e1e46d11
_woo_s.j <sjwoo1999@korea.ac.kr>
2024-08-21 01:01:32 +0900

    🔨 New Branch: Character: Initial Setting
    New Branch: Character: Initial Setting

--- src/App.js
+++ src/App.js
@@ -71,7 +71,7 @@ function AppContent() {
   const [accessToken, setAccessToken] = useState(null);
   const [refreshToken, setRefreshToken] = useState(null);
   const [loginType, setLoginType] = useState("");
-  const [userId, setUserId] = useState("");
+  const [userId, setUserId] = useState("1234");
 
   const location = useLocation();
   const navigate = useNavigate();
